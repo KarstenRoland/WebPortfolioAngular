70eb4130ae58be95e0c5daf102694d65eb8aceec
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2023-05-05 00:37:53 -0500

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { BottomBarComponent } from './bottom-bar.component'
+import { CodeExampleComponent } from './code-example.component'
 import { PageIntroComponent } from './page-intro.component'
 import { SkillsSectionComponent } from './skills-section.component'
+import { SubmitFormComponent } from './submit-form.component'
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog'
 import { MatExpansionModule } from '@angular/material/expansion'
+import { MatGridListModule } from '@angular/material/grid-list'
 import { MatProgressBarModule } from '@angular/material/progress-bar'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
---
+++ src/app/submit-form.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'submit-form',
+  templateUrl: './submit-form.component.html'
+})
+export class SubmitFormComponent {}
