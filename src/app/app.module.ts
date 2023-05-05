c8bdcfd3dd7ba573aafbfc274c41c29fd83ef986
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2023-05-04 22:36:37 -0500

    Skill section init completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { PageIntroComponent } from './page-intro.component'
-import { SkillsSectionComponent } from './skills-section.component';
+import { SkillsSectionComponent } from './skills-section.component'
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { MatExpansionModule } from '@angular/material/expansion'
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar'
 
 @NgModule({
   declarations: [
